bad1a621431f8c423780ea78015954ab2a68ec02
fe-berrios <fe.berrios.ov@gmail.com>
2026-01-26 20:37:05 -0300

    Avance en clases

--- src/main/java/bo/felipe/app/client/IDatabaseC.java
+++ src/main/java/bo/felipe/app/client/IDatabaseC.java
@@ -4,13 +4,20 @@ import bo.felipe.app.model.Venta;
 import bo.felipe.app.model.VentaRequest;
 import bo.felipe.app.model.VentaResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @FeignClient(name = "svc-database", url = "http://localhost:8083")
 public interface IDatabaseC {
 
     @PostMapping("/db/add/venta")
     VentaResponse addVenta(@RequestBody Venta venta);
+
+    @GetMapping("/db/venta/bo/{buy_order}")
+    List<VentaResponse> getVenta(@PathVariable String buy_order);
 
 }
